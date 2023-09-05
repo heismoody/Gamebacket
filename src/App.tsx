471233f2d8079f9b26adc92aad721760f1fd42b2
Heismoody <moodyamin531@gmail.com>
2023-09-05 14:36:08 +0300

    typo change

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
     <div className='flex justify-center items-center w-full h-screen'>
       <div className='text-center'>
         <strong className='text-4xl block text-blue-500'>GAMEBACKET</strong>
-        <span className='block text-xl font-medium'>Coming soon. Still Under Constraction 🚧</span>
+        <span className='block text-xl font-medium'>Coming soon. Still Under Construction 🚧</span>
       </div>
     </div>
   )
