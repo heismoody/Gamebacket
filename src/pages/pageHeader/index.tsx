44475ea4dcc284bdd0cd948e000ae03fcc3d4069
Heismoody <moodyamin531@gmail.com>
2023-09-26 11:54:39 +0300

    background fix

--- src/pages/pageHeader/index.tsx
+++ src/pages/pageHeader/index.tsx
@@ -2,7 +2,7 @@ import HeaderDetails from "./headerDetails";
 
 export default function PageHeader({...props}:IpageHeader) {
   return (
-    <div className={`w-full pt-56 bg-[url('${props.image}')] bg-cover bg-no-repeat relative flex justify-center`}>
+    <div className={`w-full pt-56 bg-cover bg-no-repeat relative flex justify-center`} style={{ backgroundImage: `url(${props.image})` }}>
         <div className="bg-black/50 absolute top-0 w-full h-full" />
         <div className="bg-gradient-to-t from-background absolute top-0 w-full h-full" />
         <div className="relative introheading-2 w-[1161px]">
@@ -14,3 +14,4 @@ export default function PageHeader({...props}:IpageHeader) {
     </div>
   )
 }
+
