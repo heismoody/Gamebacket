81278d2334eaf50c26a707068b23d91307d86e7a
Heismoody <moodyamin531@gmail.com>
2023-10-04 12:49:30 +0300

    explore category change

--- src/pages/home/exploreCatalog/index.tsx
+++ src/pages/home/exploreCatalog/index.tsx
@@ -2,7 +2,7 @@ import CardDetail from "../homeComponents/cardDetail";
 
 export default function ExploreCatalog() {
   return (
-    <section className="w-full flex justify-center items-center relative overflow-x-clip">
+    <section className="w-full flex justify-center items-center relative overflow-x-clip pt-32">
         <div className="w-[1161px] h-[430px] flex justify-center items-start z-50">
             <div className="w-[60%] h-[460px] ">
                 <img src="catalog-sect.jpg" alt="" className="imgrender"/>
